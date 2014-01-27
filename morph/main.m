b74d58b2cdf59554c7c573e8ddf1369f5359fff6
Jeremy <jeremy@macbook4.local>
2014-01-27 02:31:41 -0500

    Initial Commit

---
+++ morph/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  morph
+//
+//  Created by Jeremy on 1/27/14.
+//  Copyright (c) 2014 Jeremy March. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
